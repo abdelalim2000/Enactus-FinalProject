4a03b5516c758546426d46669dc798e6f1f3bfc9
Mohamed <mohamed.abdelalilm2000@gmail.com>
2020-09-05 20:50:29 +0200

    First Commet
    -Finished navbar
    -finished hero
    -finished app and recruitment modals

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/**
+ * NavBar Control start
+ * */
+const navBar = document.getElementById('navbar');
+const menu = document.querySelector('.menu');
+const collapseNavbar = document.getElementById('collapse-navbar');
+const navItem = document.querySelectorAll('.navitem');
+
+window.addEventListener('scroll', function () {
+    navBar.classList.toggle('scrolled', window.screenY >= 70)
+});
+
+menu.addEventListener('click', function () {
+    collapseNavbar.classList.toggle('open')
+});
+
+navItem.forEach(function (item) {
+    item.addEventListener('click', function () {
+        collapseNavbar.classList.remove('open')
+    })
+});
+
+/**
+ * NavBar Control end
+ * */
+
+/**
+ * modal control start
+ * */
+
+const modalBTN = document.querySelectorAll('.modal-trigger');
+modalBTN.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        const modal = btn.getAttribute('data-trigger');
+        document.getElementById(modal).classList.add('open')
+    })
+});
+
+
+/**
+ * join modal start
+ * */
+const joinModal = document.getElementById('join-modal');
+joinModal.addEventListener('click', function (e) {
+    if (e.target.classList.contains('modal')) {
+        joinModal.classList.remove('open')
+    }
+});
+/**
+ * join modal end
+ * */
+
+/**
+ * app modal start
+ * */
+const appModal = document.getElementById('app-modal')
+appModal.addEventListener('click', function (e) {
+    if (e.target.classList.contains('modal')){
+        appModal.classList.remove('open')
+    }
+})
+/**
+ * app modal end
+ * */
+
+/**
+ * modal control end
+ * */
